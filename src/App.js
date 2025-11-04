cfe63bbbb515c7a846d261adee84f9890f27dec5
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-04 09:07:28 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header__logo">
+        <LocalGroceryStoreIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">Cartify</h2>
+      </div>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" placeholder="Search for products..." />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign in</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Cart</span>
+        </div>
+
+        <div className="nav__item">
+          <ShoppingBasketIcon />
+          <span className="nav__itemLineTwo">0</span>
+        </div>
+
+
+      </div>
     </div>
   );
 }
