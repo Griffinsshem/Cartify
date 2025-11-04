fc20017e7345429bc4571cca33738913a4e0587e
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-04 09:55:07 +0300

    feat: create the header component with all features

---
+++ src/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Header.css';
+// Importing Material UI icons
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <LocalGroceryStoreIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">Cartify</h2>
+      </div>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" placeholder="Search for products..." />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign in</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Cart</span>
+        </div>
+
+        <div className="nav__item">
+          <ShoppingBasketIcon className="itemBasket" />
+          <span className="nav__itemLineTwo nav__basketCount">0</span>
+        </div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default Header;
