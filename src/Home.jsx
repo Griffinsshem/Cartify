04e71f91afc1bfee1e64b47d843707ad8ef30eee
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-20 10:42:02 +0300

    feat(home): add homepage product items and layout improvements

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,24 +14,96 @@ function Home() {
           />
 
           <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <Product
+              id="12321341"
+              title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+              price={29.99}
+              rating={5}
+              image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            />
+            <Product
+              id="49538094"
+              title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+              price={239.0}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/61etD4-IrPL._AC_SX679_.jpg"
+            />
+            <Product
+              id="4903850"
+              title="Samsung Galaxy Watch 4 (Bluetooth, 44mm) – Silver"
+              price={199.99}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            />
+            <Product
+              id="23445930"
+              title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+              price={98.99}
+              rating={5}
+              image="https://images-na.ssl-images-amazon.com/images/I/61Gob-M3snL._AC_SX679_.jpg"
+            />
           </div>
 
           <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <Product
+              id="3254354345"
+              title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+              price={598.99}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            />
+            <Product
+              id="90829332"
+              title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+              price={29.99}
+              rating={5}
+              image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            />
+            <Product
+              id="90829332"
+              title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+              price={239.0}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/61etD4-IrPL._AC_SX679_.jpg"
+            />
+            <Product
+              id="90829332"
+              title="Samsung Galaxy Watch 4 (Bluetooth, 44mm) – Silver"
+              price={199.99}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            />
           </div>
 
           <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <Product
+              id="90829332"
+              title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+              price={98.99}
+              rating={5}
+              image="https://images-na.ssl-images-amazon.com/images/I/61Gob-M3snL._AC_SX679_.jpg"
+            />
+            <Product
+              id="90829332"
+              title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+              price={598.99}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+            />
+            <Product
+              id="90829332"
+              title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+              price={29.99}
+              rating={5}
+              image="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            />
+            <Product
+              id="90829332"
+              title="Samsung Galaxy Watch 4 (Bluetooth, 44mm) – Silver"
+              price={199.99}
+              rating={4}
+              image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+            />
           </div>
 
         </div>
