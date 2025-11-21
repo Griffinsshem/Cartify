a11ec5ea565f72c682cceb36c8396a8c6b35d9d0
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-21 09:08:36 +0300

    feat: create the checkout page component

---
+++ src/Checkout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Checkout.css';
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <h1>Checkout Page</h1>
+      <p>Your order summary will be displayed here.</p>
+    </div>
+  );
+}
+
+export default Checkout;
