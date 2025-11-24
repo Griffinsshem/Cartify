91ca74ab9d364449643fe376dff71518c88c0ffb
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-24 22:42:16 +0300

    feat: wrap "Hello Guest" sign-in section in Link for navigation

--- src/Header.jsx
+++ src/Header.jsx
@@ -23,11 +23,12 @@ function Header() {
       </div>
 
       <div className="header__nav">
-
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign in</span>
-        </div>
+        <Link to="/login" style={{ textDecoration: "none" }}>
+          <div className="nav__item">
+            <span className="nav__itemLineOne">Hello Guest</span>
+            <span className="nav__itemLineTwo">Sign in</span>
+          </div>
+        </Link>
 
         <div className="nav__item">
           <span className="nav__itemLineOne">Your</span>
