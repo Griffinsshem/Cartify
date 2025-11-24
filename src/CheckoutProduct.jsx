7363228d383fe467501a3b565578d07174e148e5
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-24 15:04:50 +0300

    feat: refactor CheckoutProduct component to accept props for dynamic rendering

--- src/CheckoutProduct.jsx
+++ src/CheckoutProduct.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./CheckoutProduct.css";
 
-function CheckoutProduct() {
+function CheckoutProduct({ id, title, image, price, rating }) {
   return (
     <div className="checkoutProduct">
-      <img src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt="" className="checkoutProduct__image" />
+      <img src={image} alt="" className="checkoutProduct__image" />
       <div className="checkoutProduct__info">
-        <p className="checkoutProduct__title">Bennet Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)</p>
+        <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
           <small>$</small>
-          <strong>11.96</strong>
+          <strong>{price}</strong>
         </p>
         <div className="checkoutProduct__rating">
-          ⭐⭐⭐⭐⭐
+          {Array(rating).fill().map((_, i) => (
+            <p key={i}>⭐</p>
+          ))}
         </div>
         <button>Remove from Basket</button>
       </div>
