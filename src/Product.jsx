b863b98879dc9d49677ab3941f5cca4c5b3449b4
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-24 14:51:37 +0300

    feat: implement addToBasket function to dispatch items to the basket

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
 
 function Product({ id, title, price, rating, image }) {
+
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div>
       <div className="product">
@@ -24,7 +42,7 @@ function Product({ id, title, price, rating, image }) {
           src={image}
         />
 
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
       </div>
     </div>
   );
