3277e49c9f8aca742789e54adf9b53dc2bf2e6b5
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-24 14:51:43 +0300

    feat: add reducer to manage application state and handle ADD_TO_BASKET action

---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+//we define all application state changes in this file
+export const initialState = {
+  basket: [],
+};
+
+//selector
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
