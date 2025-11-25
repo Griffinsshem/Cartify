787d8ccbd4964beaa2c76015e5e2654145ba4b2d
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-11-25 10:24:29 +0300

    chore(firebase): remove hardcoded config and load Firebase settings from .env

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA41iWrlxlSFDPYL_sVVoiRnyZKYlgo4Mw",
-  authDomain: "caritfy.firebaseapp.com",
-  projectId: "caritfy",
-  storageBucket: "caritfy.firebasestorage.app",
-  messagingSenderId: "123465424382",
-  appId: "1:123465424382:web:7ce887ed82fa0f45c774af",
-  measurementId: "G-4TXCFTD4G8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
